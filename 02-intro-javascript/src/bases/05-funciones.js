7b2ba0ec489d7362ae53516a9c144d000de73050
JUANJSC630 <juanriderbmx2@gmail.com>
2022-05-27 17:34:47 -0500

    Avances de curso

---
+++ 02-intro-javascript/src/bases/05-funciones.js
@@ -0,0 +1,47 @@
+const saludar = function (nombre) {
+    return `hola ${nombre}`;
+};
+
+const saludar2 = (nombre) => {
+    return `hola ${nombre}`;
+};
+
+const saludar3 = (nombre) => `hola ${nombre}`;
+
+const saludar4 = () => `hola mundo`;
+// console.log(saludar('Goku'));
+
+const getUser = () =>
+    ({
+        uid : 'ABC123',
+        username: 'El_tramas'
+    });
+//tarea 
+//1. Transformar a funcion de flecha
+//2. tiene que retornar un objeto implisito
+function getUsuarioActivo(nombre) {
+    return{
+        uid:'ABC123456',
+        username:'Juan123'
+    }
+};
+
+//Resultado
+const GetUsuarioActivo = (nombre) => ({
+        uid:'ABC123456',
+        username:nombre
+});
+
+
+
+
+
+const usuarioActivo = getUsuarioActivo('Fernando');
+console.log(usuarioActivo);
+console.log(saludar('goku'));
+console.log(saludar2('vegeta'));
+console.log(saludar3('broly'));
+console.log(saludar4());
+
+console.log(getUser());
+console.log(GetUsuarioActivo('Mario'));
